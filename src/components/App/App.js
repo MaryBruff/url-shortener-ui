34feb31f66849c6eb1b8c7128b995a5d6705f4aa
Mary Bruff <marybruff5@gmail.com>
2024-01-16 10:22:40 -0700

    create post function

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import { getUrls } from "../../apiCalls";
+import { getUrls, postUrl } from "../../apiCalls";
 import UrlContainer from "../UrlContainer/UrlContainer";
 import UrlForm from "../UrlForm/UrlForm";
 
@@ -11,13 +11,18 @@ function App() {
     getUrls().then((data) => setUrls(data.urls));
   }, []);
 
+  const addUrl = (longUrl, title) => {
+    postUrl(longUrl, title)
+    .then((data) => setUrls([...urls, data]))
+    .catch((error) => console.log(error));
+  }
+  
   return (
     <main className="App">
       <header>
         <h1>URL Shortener</h1>
-        <UrlForm />
+        <UrlForm addUrl={addUrl} />
       </header>
-
       <UrlContainer urls={urls} />
     </main>
   );
