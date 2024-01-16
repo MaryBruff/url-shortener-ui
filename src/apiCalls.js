a5706d394fd58a05c92f2cd1d6d43991483ae893
Mary Bruff <marybruff5@gmail.com>
2024-01-16 10:22:06 -0700

    add post request

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,3 +3,18 @@ export const getUrls = () => {
     .then((response) => response.json())
     .catch((error) => console.log(error));
 };
+
+export const postUrl = (longUrl, title) => {
+  return fetch("http://localhost:3001/api/v1/urls", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      long_url: longUrl,
+      title: title,
+    }),
+  })
+    .then((response) => response.json())
+    .catch((error) => console.log(error));
+}
