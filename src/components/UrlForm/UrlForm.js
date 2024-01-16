6fb6810fc9ee87dec78e85ef9c94472883b14f75
Mary Bruff <marybruff5@gmail.com>
2024-01-16 10:02:49 -0700

    fix onChange

--- src/components/UrlForm/UrlForm.js
+++ src/components/UrlForm/UrlForm.js
@@ -1,42 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function UrlForm() {
-  const [title, setTitle] = useState('');
-  const [urlToShorten, setUrlToShorten] = useState('');
+  const [title, setTitle] = useState("");
+  const [urlToShorten, setUrlToShorten] = useState("");
 
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     clearInputs();
-  }
+  };
 
   const clearInputs = () => {
-    setTitle('');
-    setUrlToShorten('');
-  }
+    setTitle("");
+    setUrlToShorten("");
+  };
 
   return (
     <form>
       <input
-        type='text'
-        placeholder='Title...'
-        name='title'
+        type="text"
+        placeholder="Title..."
+        name="title"
         value={title}
-        // onChange={e => }
+        onChange={(e) => {
+          setTitle(e.target.value);
+        }}
       />
 
       <input
-        type='text'
-        placeholder='URL to Shorten...'
-        name='title'
-        value={title}
-        // onChange={e => }
+        type="text"
+        placeholder="URL to Shorten..."
+        name="urlToShorten"
+        value={urlToShorten}
+        onChange={(e) => {
+          setUrlToShorten(e.target.value);
+        }}
       />
 
-      <button onClick={e => handleSubmit(e)}>
-        Shorten Please!
-      </button>
+      <button onClick={(e) => handleSubmit(e)}>Shorten Please!</button>
     </form>
-  )
+  );
 }
 
 export default UrlForm;
